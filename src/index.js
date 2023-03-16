933db5d45eb7d7d947c181ca72b5199f39cc38bc
HieuNguyen1905 <120067573+HieuNguyen1905@users.noreply.github.com>
2023-03-15 21:27:02 -0700

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthContext, AuthContextProvider } from './context/authContext';
+import { AuthContextProvider } from './context/authContext';
 
 
 
